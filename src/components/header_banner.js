d04892e341b06a8c46e33b81735cc773291c5251
Arjun-Neopraxis <arjun.s@neopraxis.in>
2024-02-12 10:53:29 +0530

    Header banner

---
+++ src/components/header_banner.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { useState, useEffect } from 'react';
+ 
+export default function HeaderBanner() {
+    const [contentIndex, setContentIndex] = useState(0);
+    const contents = ['we are here for you', 'Your search ends here', 'the new era of BC']; // Add your desired contents here
+  
+    useEffect(() => {
+      const intervalId = setInterval(() => {
+        // Update content index every 1 second
+        setContentIndex((prevIndex) => (prevIndex + 1) % contents.length);
+      }, 3000);
+   
+      // Clear the interval when the component is unmounted
+      return () => clearInterval(intervalId);
+    }, [contentIndex, contents.length]);
+
+  return (
+    <div className="header-banner">
+      <h6 className='banner-header' style={{color: 'white', backgroundColor: 'black', textAlign: 'center', padding: '5px'}}>{contents[contentIndex]}</h6>
+    </div>
+  );
+}
