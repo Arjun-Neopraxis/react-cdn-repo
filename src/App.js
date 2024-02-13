8d7b9cd8b6681ff9eb03ba84f6bf527c1986e2d4
Arjun-Neopraxis <arjun.s@neopraxis.in>
2024-02-13 17:38:16 +0530

    routes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import HeaderBanner from './components/header_banner';
 import MyReactComponent from './MyReactComponent';
-
+import { BrowserRouter,Routes,Route } from 'react-router-dom';
 
 function App() {
   return (
