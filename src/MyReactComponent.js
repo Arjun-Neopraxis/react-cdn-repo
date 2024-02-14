1b8954a368f9a8f41419f392718ad6e6078ec960
Arjun-Neopraxis <arjun.s@neopraxis.in>
2024-02-14 15:11:07 +0530

    Webpack change

--- src/MyReactComponent.js
+++ src/MyReactComponent.js
@@ -3,7 +3,8 @@ import React from 'react';
 const MyReactComponent = () => {
     return (
       <div>
-        <h1 style={{color: 'white', backgroundColor: 'black'}}>Hello from MyReactComponent!</h1>
+        <h5 style={{color: 'white', backgroundColor: 'black'}}>Hello from MyReactComponent!</h5>
+        <h5 style={{color: 'white', backgroundColor: 'red'}}>Hello Rendering second data</h5>
       </div>
     );
   };
