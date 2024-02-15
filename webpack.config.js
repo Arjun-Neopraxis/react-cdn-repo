292b2d84b4b9c751b5a62a3c9d402412adf43651
Arjun-Neopraxis <arjun.s@neopraxis.in>
2024-02-15 10:55:26 +0530

    two webpack components

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 
-module.exports = {
+module.exports = [
+  {
   mode: 'production',
   entry: './src/MyReactComponent.js',
   output: {
@@ -25,4 +26,31 @@ module.exports = {
       },
     ],
   },
-};
+},
+  {
+  mode: 'production',
+  entry: './src/header_banner.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'HeaderBanner.js',
+    library: 'HeaderBanner',
+    libraryTarget: 'umd',
+    umdNamedDefine: true,
+    globalObject: 'this', // Define the global object used
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react'],
+          },
+        },
+      },
+    ],
+  },
+},
+];
