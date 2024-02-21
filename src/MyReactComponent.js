b9147c68f612515a7fc9ac509c92cbfcd6556c64
Arjun-Neopraxis <arjun.s@neopraxis.in>
2024-02-21 11:38:02 +0530

    basic js button

--- src/MyReactComponent.js
+++ src/MyReactComponent.js
@@ -1,10 +1,15 @@
 import React from 'react';
 
 const MyReactComponent = () => {
+  var buttonElement = document.getElementById('myButton');
+  buttonElement.addEventListener("click", function() {
+    console.log("button clicked");
+  })
   
     return (
       <div>
         <h6 style={{color: 'white', backgroundColor: 'black'}}>Text from MyReactComponent!</h6>
+      <button id='myButton'>Click here</button>
       </div>
     );
   };
